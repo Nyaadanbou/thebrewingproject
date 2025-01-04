3604df13efb4c9c37185f999d5a616ef63a62319
Thorinwasher <ragnarlol96@gmail.com>
2025-01-04 17:54:22 +0100

    fix(structures): Listen for breaking of placedstructures

--- src/main/java/dev/jsinco/brewery/listeners/BlockEventListener.java
+++ src/main/java/dev/jsinco/brewery/listeners/BlockEventListener.java
@@ -10,9 +10,12 @@ import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.block.*;
+import org.bukkit.event.entity.EntityChangeBlockEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
@@ -26,7 +29,7 @@ public class BlockEventListener implements Listener {
         this.placedStructureRegistry = placedStructureRegistry;
     }
 
-    @EventHandler(priority = EventPriority.MONITOR)
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onBlockPlace(BlockPlaceEvent event) {
         Optional<PlacedBreweryStructure> possibleStructure = getStructure(event.getBlock());
         if (possibleStructure.isEmpty()) {
@@ -51,5 +54,65 @@ public class BlockEventListener implements Listener {
             }
         }
         return Optional.empty();
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onBlockBreak(BlockBreakEvent event) {
+        Optional<PlacedBreweryStructure> placedBreweryStructure = placedStructureRegistry.getStructure(event.getBlock().getLocation());
+        placedBreweryStructure.ifPresent(placedStructureRegistry::removeStructure);
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onPistonExtend(BlockPistonExtendEvent event) {
+        onMultiBlockRemove(event.getBlocks().stream()
+                .map(block -> block.getRelative(event.getDirection()))
+                .map(Block::getLocation)
+                .toList());
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onPistonRetract(BlockPistonRetractEvent event) {
+        onMultiBlockRemove(event.getBlocks().stream()
+                .map(block -> block.getRelative(event.getDirection()))
+                .map(Block::getLocation)
+                .toList()
+        );
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onEntityExplode(EntityExplodeEvent event) {
+        onMultiBlockRemove(event.blockList().stream()
+                .map(Block::getLocation)
+                .toList()
+        );
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onBlockExplode(BlockExplodeEvent event) {
+        onMultiBlockRemove(event.blockList().stream()
+                .map(Block::getLocation)
+                .toList()
+        );
+    }
+
+    private void onMultiBlockRemove(List<Location> locationList) {
+        Set<PlacedBreweryStructure> placedBreweryStructures = new HashSet<>();
+        for (Location location : locationList) {
+            Optional<PlacedBreweryStructure> placedBreweryStructure = placedStructureRegistry.getStructure(location);
+            placedBreweryStructure.ifPresent(placedBreweryStructures::add);
+        }
+        placedBreweryStructures.forEach(placedStructureRegistry::removeStructure);
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onBlockBurn(BlockBurnEvent event) {
+        Optional<PlacedBreweryStructure> placedBreweryStructure = placedStructureRegistry.getStructure(event.getBlock().getLocation());
+        placedBreweryStructure.ifPresent(placedStructureRegistry::removeStructure);
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onEntityChangeBlock(EntityChangeBlockEvent event) {
+        Optional<PlacedBreweryStructure> placedBreweryStructure = placedStructureRegistry.getStructure(event.getBlock().getLocation());
+        placedBreweryStructure.ifPresent(placedStructureRegistry::removeStructure);
     }
 }
