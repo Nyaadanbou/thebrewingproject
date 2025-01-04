af890f8bcd2ca2a5eded1b4386b24831d5a49d6f
Thorinwasher <ragnarlol96@gmail.com>
2025-01-04 13:32:43 +0100

    fix(structure): Optimize structure localization

--- src/main/java/dev/jsinco/brewery/structure/PlacedBreweryStructure.java
+++ src/main/java/dev/jsinco/brewery/structure/PlacedBreweryStructure.java
@@ -4,24 +4,16 @@ import org.bukkit.Location;
 import org.joml.Matrix3d;
 import org.joml.Vector3i;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public record PlacedBreweryStructure(BreweryStructure structure, Matrix3d transformation,
                                      Location worldOrigin, Vector3i structureOrigin) {
+    private static final List<Matrix3d> ALLOWED_TRANSFORMATIONS = compileAllowedTransformations();
 
     public static Optional<PlacedBreweryStructure> findValid(BreweryStructure structure, Location worldOrigin) {
-        Matrix3d transformation = new Matrix3d();
-        for (int i = 0; i < 4; i++) {
-            transformation.rotate(Math.PI / 2, 0, 1, 0);
-            Optional<Vector3i> possibleOrigin = structure.findValidOrigin(transformation, worldOrigin);
-            if (possibleOrigin.isPresent()) {
-                return possibleOrigin.map(origin -> new PlacedBreweryStructure(structure, transformation, worldOrigin, origin));
-            }
-        }
-        transformation.reflect(1, 0, 0);
-        for (int i = 0; i < 4; i++) {
-            transformation.rotate(Math.PI / 2, 0, 1, 0);
+        for (Matrix3d transformation : ALLOWED_TRANSFORMATIONS) {
             Optional<Vector3i> possibleOrigin = structure.findValidOrigin(transformation, worldOrigin);
             if (possibleOrigin.isPresent()) {
                 return possibleOrigin.map(origin -> new PlacedBreweryStructure(structure, transformation, worldOrigin, origin));
@@ -33,5 +25,18 @@ public record PlacedBreweryStructure(BreweryStructure structure, Matrix3d transf
     public List<Location> getPositions() {
         return List.copyOf(structure.getExpectedBlocks(transformation, worldOrigin, structureOrigin)
                 .keySet());
+    }
+
+    private static List<Matrix3d> compileAllowedTransformations() {
+        List<Matrix3d> output = new ArrayList<>();
+        Matrix3d transformation = new Matrix3d();
+        for (int i = 0; i < 4; i++) {
+            output.add(transformation.rotate(Math.PI / 2 * i, 0, 1, 0, new Matrix3d()));
+        }
+        transformation.reflect(1, 0, 0);
+        for (int i = 0; i < 4; i++) {
+            output.add(transformation.rotate(Math.PI / 2 * i, 0, 1, 0, new Matrix3d()));
+        }
+        return List.copyOf(output);
     }
 }
