cbb5a9e6af6040b23e5eb7d6e10cd5c3089f5357
Thorinwasher <ragnarlol96@gmail.com>
2025-03-22 07:51:55 +0100

    placeholder(inventoryaccess): Placeholder item transfer denial

--- bukkit/src/main/java/dev/jsinco/brewery/bukkit/listeners/InventoryEventListener.java
+++ bukkit/src/main/java/dev/jsinco/brewery/bukkit/listeners/InventoryEventListener.java
@@ -2,22 +2,12 @@ package dev.jsinco.brewery.bukkit.listeners;
 
 import dev.jsinco.brewery.breweries.InventoryAccessible;
 import dev.jsinco.brewery.bukkit.breweries.BreweryRegistry;
-import dev.jsinco.brewery.bukkit.breweries.BukkitDistillery;
-import dev.jsinco.brewery.bukkit.breweries.BukkitDistilleryDataType;
-import dev.jsinco.brewery.bukkit.util.BukkitAdapter;
 import dev.jsinco.brewery.database.Database;
-import dev.jsinco.brewery.util.vector.BreweryLocation;
-import org.bukkit.block.BrewingStand;
+import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.BrewEvent;
-import org.bukkit.event.inventory.HopperInventorySearchEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryOpenEvent;
-import org.bukkit.inventory.BrewerInventory;
-
-import java.sql.SQLException;
-import java.util.Optional;
+import org.bukkit.inventory.ItemStack;
 
 public class InventoryEventListener implements Listener {
 
@@ -31,6 +21,12 @@ public class InventoryEventListener implements Listener {
 
     @EventHandler(ignoreCancelled = true)
     public void onInventoryClick(InventoryClickEvent event) {
-        InventoryAccessible inventoryAccessible = registry.getFromInventory(event.getInventory());
+        InventoryAccessible<ItemStack> inventoryAccessible = registry.getFromInventory(event.getClickedInventory());
+        if (inventoryAccessible == null) {
+            return;
+        }
+        if (!inventoryAccessible.inventoryAllows(event.getView().getPlayer().getUniqueId(), event.getCursor())) {
+            event.setResult(Event.Result.DENY);
+        }
     }
 }
