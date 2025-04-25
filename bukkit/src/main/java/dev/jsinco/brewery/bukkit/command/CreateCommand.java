39718ce48b7fc1e392b440d8b7fb4a84c3611129
Thorinwasher <ragnarlol96@gmail.com>
2025-04-25 10:24:43 +0200

    fix(create-command): Add unit tests + fixes

--- bukkit/src/main/java/dev/jsinco/brewery/bukkit/command/CreateCommand.java
+++ bukkit/src/main/java/dev/jsinco/brewery/bukkit/command/CreateCommand.java
@@ -8,16 +8,19 @@ import dev.jsinco.brewery.bukkit.brew.BrewAdapter;
 import dev.jsinco.brewery.bukkit.ingredient.BukkitIngredientManager;
 import dev.jsinco.brewery.configuration.locale.TranslationsConfig;
 import dev.jsinco.brewery.ingredient.Ingredient;
-import dev.jsinco.brewery.util.BreweryKey;
-import dev.jsinco.brewery.util.Registry;
 import dev.jsinco.brewery.moment.Moment;
 import dev.jsinco.brewery.moment.PassedMoment;
+import dev.jsinco.brewery.util.BreweryKey;
+import dev.jsinco.brewery.util.Registry;
+import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.*;
 import java.util.function.BiFunction;
@@ -34,7 +37,7 @@ public class CreateCommand {
     );
 
     public static boolean onCommand(Player target, CommandSender sender, String[] args) {
-        Map<String, BiFunction<Queue<String>, CommandSender, BrewingStep>> operators = Map.of(
+        Map<String, BiFunction<Queue<String>, CommandSender, @Nullable BrewingStep>> operators = Map.of(
                 "--age", CreateCommand::getAge,
                 "--distill", CreateCommand::getDistill,
                 "--cook", CreateCommand::getCook,
@@ -51,23 +54,38 @@ public class CreateCommand {
             BiFunction<Queue<String>, CommandSender, BrewingStep> operator = operators.get(operatorName);
             if (operator == null) {
                 sender.sendMessage(MiniMessage.miniMessage().deserialize(TranslationsConfig.COMMAND_CREATE_UNKNOWN_ARGUMENT, Placeholder.unparsed("argument", operatorName)));
-                return false;
+                return true;
             }
-            steps.add(operator.apply(arguments, sender));
+            BrewingStep brewingStep = operator.apply(arguments, sender);
+            if (brewingStep == null) {
+                return true;
+            }
+            steps.add(brewingStep);
         }
         ItemStack brewItem = BrewAdapter.toItem(new BrewImpl(steps), new BrewImpl.State.Other());
         target.getWorld().dropItem(target.getLocation(), brewItem);
-        sender.sendMessage(MiniMessage.miniMessage().deserialize(TranslationsConfig.COMMAND_CREATE_SUCCESS, Placeholder.component("brew_name", brewItem.effectiveName())));
+        ItemMeta brewItemMeta = brewItem.getItemMeta();
+        sender.sendMessage(MiniMessage.miniMessage().deserialize(TranslationsConfig.COMMAND_CREATE_SUCCESS, Placeholder.component("brew_name", brewItemMeta.hasCustomName() ? brewItemMeta.customName() : brewItemMeta.itemName())));
         return true;
     }
 
     private static BrewingStep getMix(Queue<String> arguments, CommandSender sender) {
-        long cookTime = (long) (Double.parseDouble(arguments.poll()) * Moment.MINUTE);
+        if (arguments.size() < 2) {
+            sender.sendMessage(missingArgument(new String[]{"cook_time", "ingredients"}[arguments.size()]));
+            return null;
+        }
+        Double cookTime = parseDouble(arguments.poll(), sender);
+        if (cookTime == null) {
+            return null;
+        }
         Map<Ingredient, Integer> ingredients = retrieveIngredients(arguments, sender);
-        return new BrewingStep.Mix(new PassedMoment(cookTime), ingredients);
+        if (ingredients == null) {
+            return null;
+        }
+        return new BrewingStep.Mix(new PassedMoment((long) (cookTime * Moment.MINUTE)), ingredients);
     }
 
-    private static Map<Ingredient, Integer> retrieveIngredients(Queue<String> arguments, CommandSender sender) {
+    private static @Nullable Map<Ingredient, Integer> retrieveIngredients(Queue<String> arguments, CommandSender sender) {
         List<String> ingredientStrings = new ArrayList<>();
         while (!arguments.isEmpty() && !arguments.peek().startsWith("-")) {
             ingredientStrings.add(arguments.poll());
@@ -77,27 +95,82 @@ public class CreateCommand {
                 .toList();
         if (!invalidIngredientArguments.isEmpty()) {
             String invalidIngredients = String.join(",", invalidIngredientArguments);
-            sender.sendMessage(MiniMessage.miniMessage().deserialize(TranslationsConfig.COMMAND_CREATE_UNKNOWN_ARGUMENT, Placeholder.unparsed("argument", invalidIngredients)));
-            throw new IllegalArgumentException("Could not find the ingredient(s): " + invalidIngredients);
+            sender.sendMessage(illegalArgument(invalidIngredients));
+            return null;
         }
         return BukkitIngredientManager.INSTANCE.getIngredientsWithAmount(ingredientStrings);
     }
 
-    private static BrewingStep getCook(Queue<String> arguments, CommandSender sender) {
-        long cookTime = (long) (Double.parseDouble(arguments.poll()) * Moment.MINUTE);
-        CauldronType cauldronType = Registry.CAULDRON_TYPE.get(BreweryKey.parse(arguments.poll()));
-        return new BrewingStep.Cook(new PassedMoment(cookTime), retrieveIngredients(arguments, sender), cauldronType);
+    private static @Nullable BrewingStep getCook(Queue<String> arguments, CommandSender sender) {
+        if (arguments.size() < 3) {
+            sender.sendMessage(missingArgument(new String[]{"cook_time", "cauldron_type", "ingredients"}[arguments.size()]));
+            return null;
+        }
+        Double cookTime = parseDouble(arguments.poll(), sender);
+        if (cookTime == null) {
+            return null;
+        }
+        String cauldronString = arguments.poll();
+        CauldronType cauldronType = Registry.CAULDRON_TYPE.get(BreweryKey.parse(cauldronString));
+        if (cauldronType == null) {
+            sender.sendMessage(illegalArgument(cauldronString));
+            return null;
+        }
+        Map<Ingredient, Integer> ingredients = retrieveIngredients(arguments, sender);
+        if (ingredients == null) {
+            return null;
+        }
+        return new BrewingStep.Cook(new PassedMoment((long) (cookTime * Moment.MINUTE)), ingredients, cauldronType);
     }
 
     private static BrewingStep getDistill(Queue<String> arguments, CommandSender sender) {
-        int distillAmount = Integer.parseInt(arguments.poll());
-        return new BrewingStep.Distill(distillAmount);
+        if (arguments.isEmpty()) {
+            sender.sendMessage(missingArgument("runs"));
+            return null;
+        }
+        String distillString = arguments.poll();
+        try {
+            int distillAmount = Integer.parseInt(distillString);
+            return new BrewingStep.Distill(distillAmount);
+        } catch (IllegalArgumentException e) {
+            sender.sendMessage(illegalArgument(distillString));
+            return null;
+        }
     }
 
     private static BrewingStep getAge(Queue<String> arguments, CommandSender sender) {
-        long age = (long) (Double.parseDouble(arguments.poll()) * Moment.AGING_YEAR);
-        BarrelType barrelType = Registry.BARREL_TYPE.get(BreweryKey.parse(arguments.poll()));
-        return new BrewingStep.Age(new PassedMoment(age), barrelType);
+        if (arguments.size() < 2) {
+            sender.sendMessage(missingArgument(new String[]{"aging_years", "barrel_type"}[arguments.size()]));
+            return null;
+        }
+        Double agingYear = parseDouble(arguments.poll(), sender);
+        if (agingYear == null) {
+            return null;
+        }
+        String barrelTypeString = arguments.poll();
+        BarrelType barrelType = Registry.BARREL_TYPE.get(BreweryKey.parse(barrelTypeString));
+        if (barrelType == null) {
+            sender.sendMessage(illegalArgument(barrelTypeString));
+            return null;
+        }
+        return new BrewingStep.Age(new PassedMoment((long) (agingYear * Moment.AGING_YEAR)), barrelType);
+    }
+
+    private static Component illegalArgument(String argument) {
+        return MiniMessage.miniMessage().deserialize(TranslationsConfig.COMMAND_CREATE_UNKNOWN_ARGUMENT, Placeholder.unparsed("argument", argument));
+    }
+
+    private static Component missingArgument(String argumentType) {
+        return MiniMessage.miniMessage().deserialize(TranslationsConfig.COMMAND_MISSING_ARGUMENT, Placeholder.unparsed("argument_type", "<" + argumentType + ">"));
+    }
+
+    private static @Nullable Double parseDouble(String argument, CommandSender sender) {
+        try {
+            return Double.parseDouble(argument);
+        } catch (IllegalArgumentException e) {
+            sender.sendMessage(illegalArgument(argument));
+            return null;
+        }
     }
 
     public static List<String> tabComplete(@NotNull String @NotNull [] args) {
