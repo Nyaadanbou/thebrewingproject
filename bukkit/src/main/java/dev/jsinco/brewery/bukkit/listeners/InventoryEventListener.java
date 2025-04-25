bc67aadd6ff41275b67f668862de3a945c3a0cf6
Thorinwasher <ragnarlol96@gmail.com>
2025-04-25 07:43:06 +0200

    fix(brew): Lore change not applying when shift clicking item

--- bukkit/src/main/java/dev/jsinco/brewery/bukkit/listeners/InventoryEventListener.java
+++ bukkit/src/main/java/dev/jsinco/brewery/bukkit/listeners/InventoryEventListener.java
@@ -59,12 +59,11 @@ public class InventoryEventListener implements Listener {
         ItemStack hoveredItem = event.getCurrentItem();
         Stream<ItemStack> relatedItems;
         if (upperInventoryIsClicked) {
-            int slot = event.getRawSlot();
-            ItemStack initial = view.getItem(slot);
-            if (initial != null) {
-                view.setItem(slot, BrewAdapter.fromItem(initial)
+            if (hoveredItem != null) {
+                BrewAdapter.fromItem(hoveredItem)
                         .map(brew -> BrewAdapter.toItem(brew, new BrewImpl.State.Other()))
-                        .orElse(initial));
+                        .map(ItemStack::getItemMeta)
+                        .ifPresent(hoveredItem::setItemMeta);
             }
         }
         if (action == InventoryAction.MOVE_TO_OTHER_INVENTORY) {
