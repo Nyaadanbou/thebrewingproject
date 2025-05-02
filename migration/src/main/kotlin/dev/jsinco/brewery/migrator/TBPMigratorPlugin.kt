77835ba6903bc1fd4a4183e92fcaa094241e086e
Hjalmar <ragnarlol96@gmail.com>
2025-05-02 11:05:09 +0200

    fix(migration): Run after initialization (avoid tick server start freezing from trying to load chunks)

--- migration/src/main/kotlin/dev/jsinco/brewery/migrator/TBPMigratorPlugin.kt
+++ migration/src/main/kotlin/dev/jsinco/brewery/migrator/TBPMigratorPlugin.kt
@@ -8,13 +8,15 @@ import org.bukkit.plugin.java.JavaPlugin
 class TBPMigratorPlugin : JavaPlugin() {
 
     override fun onEnable() {
-        Bukkit.getWorlds().forEach(BarrelMigration::migrateWorld)
-        TheBrewingProject.getInstance().database.flush().join()
-        TheBrewingProject.getInstance().reload()
-        val pluginManager = Bukkit.getPluginManager()
-        logger.info("Successfully migrated barrel and configuration data to TheBrewingProject")
-        logger.info("You can now remove both BreweryX and " + this.name + " from your plugins folder")
-        pluginManager.disablePlugin(pluginManager.getPlugin("BreweryX")!!)
-        pluginManager.disablePlugin(this)
+        Bukkit.getScheduler().runTask(this) { ->
+            Bukkit.getWorlds().forEach(BarrelMigration::migrateWorld)
+            TheBrewingProject.getInstance().database.flush().join()
+            TheBrewingProject.getInstance().reload()
+            val pluginManager = Bukkit.getPluginManager()
+            logger.info("Successfully migrated barrel and configuration data to TheBrewingProject")
+            logger.info("You can now remove both BreweryX and " + this.name + " from your plugins folder")
+            pluginManager.disablePlugin(pluginManager.getPlugin("BreweryX")!!)
+            pluginManager.disablePlugin(this)
+        }
     }
 }
