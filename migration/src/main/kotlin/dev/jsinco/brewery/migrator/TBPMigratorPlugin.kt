46f4a46451e1d1cf22d4014bdad0065a2092b57a
Hjalmar <ragnarlol96@gmail.com>
2025-05-02 10:17:38 +0200

    fix(migration): Delete world listener BreweryX does not seem to handle delayed loading of worlds, so I don't need to as well

--- migration/src/main/kotlin/dev/jsinco/brewery/migrator/TBPMigratorPlugin.kt
+++ migration/src/main/kotlin/dev/jsinco/brewery/migrator/TBPMigratorPlugin.kt
@@ -2,15 +2,14 @@ package dev.jsinco.brewery.migrator
 
 import dev.jsinco.brewery.bukkit.TheBrewingProject
 import dev.jsinco.brewery.migrator.barrel.BarrelMigration
-import dev.jsinco.brewery.migrator.listener.WorldListener
 import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 
 class TBPMigratorPlugin : JavaPlugin() {
 
     override fun onEnable() {
-        Bukkit.getPluginManager().registerEvents(WorldListener, this)
         Bukkit.getWorlds().forEach(BarrelMigration::migrateWorld)
+        TheBrewingProject.getInstance().database.flush().join()
         TheBrewingProject.getInstance().reload()
     }
 }
