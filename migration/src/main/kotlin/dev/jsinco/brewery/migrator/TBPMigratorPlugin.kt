919aec48a1d69cebaa70759320e2e141bca4276b
Hjalmar <ragnarlol96@gmail.com>
2025-05-02 10:32:12 +0200

    fix(migration): Disable plugin on enable, as it has done its function

--- migration/src/main/kotlin/dev/jsinco/brewery/migrator/TBPMigratorPlugin.kt
+++ migration/src/main/kotlin/dev/jsinco/brewery/migrator/TBPMigratorPlugin.kt
@@ -11,5 +11,10 @@ class TBPMigratorPlugin : JavaPlugin() {
         Bukkit.getWorlds().forEach(BarrelMigration::migrateWorld)
         TheBrewingProject.getInstance().database.flush().join()
         TheBrewingProject.getInstance().reload()
+        val pluginManager = Bukkit.getPluginManager()
+        logger.info("Successfully migrated barrel and configuration data to TheBrewingProject")
+        logger.info("You can now remove both BreweryX and " + this.name + " from your plugins folder")
+        pluginManager.disablePlugin(pluginManager.getPlugin("BreweryX")!!)
+        pluginManager.disablePlugin(this)
     }
 }
