4f000a169e9878645aee781db487c89f7fdba2c2
Thorinwasher <ragnarlol96@gmail.com>
2025-05-04 15:25:22 +0200

    fix(registry): Reuse code

--- api/src/main/java/dev/jsinco/brewery/util/Registry.java
+++ api/src/main/java/dev/jsinco/brewery/util/Registry.java
@@ -6,76 +6,62 @@ import dev.jsinco.brewery.breweries.CauldronType;
 import dev.jsinco.brewery.event.NamedDrunkEvent;
 import dev.jsinco.brewery.structure.StructureMeta;
 import dev.jsinco.brewery.structure.StructureType;
+import org.jetbrains.annotations.Nullable;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 
-public class Registry {
+public class Registry<T extends BreweryKeyed> {
 
-    public static final Map<BreweryKey, BarrelType> BARREL_TYPE = getBarrelTypes();
-    public static final Map<BreweryKey, CauldronType> CAULDRON_TYPE = getEnumValues();
-    public static final Map<BreweryKey, StructureMeta<?>> STRUCTURE_META = getStructureMeta();
-    public static final Map<BreweryKey, StructureType> STRUCTURE_TYPE = getStructureTypes();
-    public static final Map<BreweryKey, NamedDrunkEvent> DRUNK_EVENT = getDrunkEvents();
+    public static final Registry<BarrelType> BARREL_TYPE = fromEnums(BarrelType.class);
+    public static final Registry<CauldronType> CAULDRON_TYPE = fromEnums(CauldronType.class);
+    public static final Registry<StructureMeta<?>> STRUCTURE_META = (Registry<StructureMeta<?>>) fromFields(StructureMeta.class);
+    public static final Registry<StructureType> STRUCTURE_TYPE = (Registry<StructureType>) fromFields(StructureType.class);
+    public static final Registry<NamedDrunkEvent> DRUNK_EVENT = fromEnums(NamedDrunkEvent.class);
 
-    private static Map<BreweryKey, NamedDrunkEvent> getDrunkEvents() {
-        ImmutableMap.Builder<BreweryKey, NamedDrunkEvent> outputBuilder = ImmutableMap.builder();
-        for (NamedDrunkEvent drunkEvent : NamedDrunkEvent.values()) {
-            outputBuilder.put(drunkEvent.key(), drunkEvent);
-        }
-        return outputBuilder.build();
+    private final ImmutableMap<BreweryKey, T> backing;
+
+    private Registry(List<T> values) {
+        ImmutableMap.Builder<BreweryKey, T> registryBacking = ImmutableMap.builder();
+        values.forEach(value -> registryBacking.put(value.key(), value));
+        this.backing = registryBacking.build();
     }
 
-    private static Map<BreweryKey, StructureMeta<?>> getStructureMeta() {
+    public Collection<T> values() {
+        return backing.values();
+    }
+
+    public @Nullable T get(BreweryKey key) {
+        return backing.get(key);
+    }
+
+    public boolean containsKey(BreweryKey key) {
+        return backing.containsKey(key);
+    }
+
+    private static <E extends Enum<E> & BreweryKeyed> Registry<E> fromEnums(Class<E> enumClass) {
+        return new Registry<>(Arrays.stream(enumClass.getEnumConstants()).toList());
+    }
+
+    private static <T extends BreweryKeyed> Registry<? extends T> fromFields(Class<T> tClass) {
         try {
-            ImmutableMap.Builder<BreweryKey, StructureMeta<?>> outputBuilder = ImmutableMap.builder();
-            for (Field field : StructureMeta.class.getDeclaredFields()) {
+            List<T> tList = new ArrayList<>();
+            for (Field field : tClass.getDeclaredFields()) {
                 if (!Modifier.isStatic(field.getModifiers())) {
                     continue;
                 }
                 Object staticField = field.get(null);
-                if (staticField instanceof StructureMeta<?> structureMeta) {
-                    outputBuilder.put(structureMeta.key(), structureMeta);
+                if (tClass.isInstance(staticField)) {
+                    tList.add(tClass.cast(staticField));
                 }
             }
-            return outputBuilder.build();
+            return new Registry<>(tList);
         } catch (IllegalAccessException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    private static Map<BreweryKey, StructureType> getStructureTypes() {
-        try {
-            ImmutableMap.Builder<BreweryKey, StructureType> outputBuilder = ImmutableMap.builder();
-            for (Field field : StructureType.class.getDeclaredFields()) {
-                if (!Modifier.isStatic(field.getModifiers())) {
-                    continue;
-                }
-                Object staticField = field.get(null);
-                if (staticField instanceof StructureType structureType) {
-                    outputBuilder.put(structureType.key(), structureType);
-                }
-            }
-            return outputBuilder.build();
-        } catch (IllegalAccessException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static Map<BreweryKey, CauldronType> getEnumValues() {
-        ImmutableMap.Builder<BreweryKey, CauldronType> outputBuilder = ImmutableMap.builder();
-        for (CauldronType barrelType : CauldronType.values()) {
-            outputBuilder.put(barrelType.key(), barrelType);
-        }
-        return outputBuilder.build();
-    }
-
-    private static Map<BreweryKey, BarrelType> getBarrelTypes() {
-        ImmutableMap.Builder<BreweryKey, BarrelType> outputBuilder = ImmutableMap.builder();
-        for (BarrelType barrelType : BarrelType.values()) {
-            outputBuilder.put(barrelType.key(), barrelType);
-        }
-        return outputBuilder.build();
     }
 }
