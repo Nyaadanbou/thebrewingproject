9041bdad92be2774d9354099338bdd1addbc4d20
Thorinwasher <ragnarlol96@gmail.com>
2025-07-11 09:37:00 +0200

    fix(future): Simplify merging of futures

--- core/src/main/java/dev/jsinco/brewery/util/FutureUtil.java
+++ core/src/main/java/dev/jsinco/brewery/util/FutureUtil.java
@@ -1,6 +1,5 @@
 package dev.jsinco.brewery.util;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
@@ -11,16 +10,7 @@ public class FutureUtil {
     }
 
     public static <T> CompletableFuture<List<T>> mergeFutures(List<CompletableFuture<T>> completableFutureList) {
-        if (completableFutureList.isEmpty()) {
-            return CompletableFuture.completedFuture(List.of());
-        }
-        T[] unwrappedFutures = (T[]) new Object[completableFutureList.size()];
-        CompletableFuture<?>[] completableFutures = new CompletableFuture<?>[completableFutureList.size()];
-        for (int i = 0; i < completableFutureList.size(); i++) {
-            final int iFinal = i;
-            completableFutures[i] = completableFutureList.get(i).thenAcceptAsync(t -> unwrappedFutures[iFinal] = t);
-        }
-        return CompletableFuture.allOf(completableFutures)
-                .thenApplyAsync(ignored -> Arrays.asList(unwrappedFutures));
+        return CompletableFuture.allOf(completableFutureList.toArray(CompletableFuture<?>[]::new))
+                .thenApplyAsync(ignored -> completableFutureList.stream().map(CompletableFuture::join).toList());
     }
 }
