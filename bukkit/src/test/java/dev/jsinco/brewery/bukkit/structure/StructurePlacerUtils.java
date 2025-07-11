9839215b60de364611214c458b1a1de6eb931bbf
Nadwey <nadweyyy@gmail.com>
2025-07-11 16:53:55 +0200

    test: Update MockBukkit to v4.64.0
    * Update MockBukkit to v4.64.0
    
    * Update Minecraft to 1.21.7
    
    * Replace deprecated `BlockDataMock.mock` with `BlockDataMockFactory.mock`

--- bukkit/src/test/java/dev/jsinco/brewery/bukkit/structure/StructurePlacerUtils.java
+++ bukkit/src/test/java/dev/jsinco/brewery/bukkit/structure/StructurePlacerUtils.java
@@ -4,7 +4,7 @@ import org.bukkit.Material;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.data.Bisected;
 import org.bukkit.block.data.type.Fence;
-import org.mockbukkit.mockbukkit.block.data.BlockDataMock;
+import org.mockbukkit.mockbukkit.block.data.BlockDataMockFactory;
 import org.mockbukkit.mockbukkit.block.data.StairsDataMock;
 import org.mockbukkit.mockbukkit.block.data.WallSignDataMock;
 import org.mockbukkit.mockbukkit.world.WorldMock;
@@ -17,28 +17,28 @@ import java.nio.file.Paths;
 public class StructurePlacerUtils {
 
     public static void constructSmallOakBarrel(WorldMock worldMock) {
-        StairsDataMock stairsDataMock1 = (StairsDataMock) BlockDataMock.mock(Material.SPRUCE_STAIRS);
+        StairsDataMock stairsDataMock1 = (StairsDataMock) BlockDataMockFactory.mock(Material.SPRUCE_STAIRS);
         stairsDataMock1.setFacing(BlockFace.EAST);
         stairsDataMock1.setHalf(Bisected.Half.TOP);
         worldMock.setBlockData(-3, 1, 2, stairsDataMock1);
         worldMock.setBlockData(-3, 1, 3, stairsDataMock1);
         worldMock.setBlockData(-3, 1, 4, stairsDataMock1);
 
-        StairsDataMock stairsDataMock2 = (StairsDataMock) BlockDataMock.mock(Material.SPRUCE_STAIRS);
+        StairsDataMock stairsDataMock2 = (StairsDataMock) BlockDataMockFactory.mock(Material.SPRUCE_STAIRS);
         stairsDataMock2.setFacing(BlockFace.WEST);
         stairsDataMock2.setHalf(Bisected.Half.TOP);
         worldMock.setBlockData(-2, 1, 2, stairsDataMock2);
         worldMock.setBlockData(-2, 1, 3, stairsDataMock2);
         worldMock.setBlockData(-2, 1, 4, stairsDataMock2);
 
-        StairsDataMock stairsDataMock3 = (StairsDataMock) BlockDataMock.mock(Material.SPRUCE_STAIRS);
+        StairsDataMock stairsDataMock3 = (StairsDataMock) BlockDataMockFactory.mock(Material.SPRUCE_STAIRS);
         stairsDataMock3.setFacing(BlockFace.EAST);
         stairsDataMock3.setHalf(Bisected.Half.BOTTOM);
         worldMock.setBlockData(-3, 2, 2, stairsDataMock3);
         worldMock.setBlockData(-3, 2, 3, stairsDataMock3);
         worldMock.setBlockData(-3, 2, 4, stairsDataMock3);
 
-        StairsDataMock stairsDataMock4 = (StairsDataMock) BlockDataMock.mock(Material.SPRUCE_STAIRS);
+        StairsDataMock stairsDataMock4 = (StairsDataMock) BlockDataMockFactory.mock(Material.SPRUCE_STAIRS);
         stairsDataMock4.setFacing(BlockFace.WEST);
         stairsDataMock4.setHalf(Bisected.Half.BOTTOM);
         worldMock.setBlockData(-2, 2, 2, stairsDataMock4);
@@ -51,9 +51,9 @@ public class StructurePlacerUtils {
     }
 
     public static void constructBambooDistillery(WorldMock worldMock) {
-        Fence bamboo1 = (Fence) BlockDataMock.mock(Material.BAMBOO_FENCE);
+        Fence bamboo1 = (Fence) BlockDataMockFactory.mock(Material.BAMBOO_FENCE);
         bamboo1.setFace(BlockFace.EAST, true);
-        Fence bamboo2 = (Fence) BlockDataMock.mock(Material.BAMBOO_FENCE);
+        Fence bamboo2 = (Fence) BlockDataMockFactory.mock(Material.BAMBOO_FENCE);
         bamboo1.setFace(BlockFace.WEST, true);
         worldMock.setBlockData(0, 1, 0, Material.DECORATED_POT.createBlockData());
         worldMock.setBlockData(0, 2, 0, Material.DECORATED_POT.createBlockData());
