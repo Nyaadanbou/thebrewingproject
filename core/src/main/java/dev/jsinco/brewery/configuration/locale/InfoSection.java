d7a8b1804b69e7684f890d3255940caee6fa2363
Thorinwasher <ragnarlol96@gmail.com>
2025-07-12 11:15:27 +0200

    revert(translations): Use configurate on translations

--- core/src/main/java/dev/jsinco/brewery/configuration/locale/InfoSection.java
+++
@@ -1,9 +0,0 @@
-package dev.jsinco.brewery.configuration.locale;
-
-import org.spongepowered.configurate.objectmapping.ConfigSerializable;
-
-@ConfigSerializable
-public record InfoSection(
-        String afterDrink
-) {
-}
