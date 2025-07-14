5c09accf209255e31bbd1e940e18740a4a3e9ffd
Mitality <96748385+Mitality@users.noreply.github.com>
2025-07-14 13:53:06 +0200

    refactor(PukeHandler): replace deprecated Bukkit.spigot().getConfig() with manual spigot.yml loading

--- bukkit/src/main/java/dev/jsinco/brewery/bukkit/effect/event/PukeHandler.java
+++ bukkit/src/main/java/dev/jsinco/brewery/bukkit/effect/event/PukeHandler.java
@@ -10,6 +10,7 @@ import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.scheduler.BukkitTask;
 import org.bukkit.util.Vector;
 
+import java.io.File;
 import java.util.Random;
 
 public class PukeHandler {
@@ -44,7 +45,9 @@ public class PukeHandler {
         item.getPersistentDataContainer().set(PUKE_ITEM, PersistentDataType.BOOLEAN, true);
 
         World world = loc.getWorld();
-        YamlConfiguration spigotConfig = Bukkit.spigot().getConfig();
+
+        File spigotYml = new File(Bukkit.getServer().getWorldContainer(), "spigot.yml");
+        YamlConfiguration spigotConfig = YamlConfiguration.loadConfiguration(spigotYml);
         int worldDespawnRate = spigotConfig.getInt("world-settings." + world.getName() + ".item-despawn-rate", -1);
         if (worldDespawnRate < 0) {
             worldDespawnRate = spigotConfig.getInt("world-settings.default.item-despawn-rate", 6000);
@@ -52,4 +55,5 @@ public class PukeHandler {
         int despawnRate = Math.max(Config.config().puke().pukeDespawnTime(), 4);
         item.setTicksLived(worldDespawnRate - despawnRate + RANDOM.nextInt(-despawnRate / 2, despawnRate / 2 + 1));
     }
+
 }
