f7e7f37fc42455f710f1170526707fe5338cb350
Thorinwasher <ragnarlol96@gmail.com>
2025-07-16 16:43:27 +0200

    fix(blurred-speech): Make it configurable

--- core/src/main/java/dev/jsinco/brewery/configuration/EventSection.java
+++ core/src/main/java/dev/jsinco/brewery/configuration/EventSection.java
@@ -8,6 +8,7 @@ import dev.jsinco.brewery.vector.BreweryLocation;
 import lombok.Getter;
 import lombok.experimental.Accessors;
 import org.spongepowered.configurate.objectmapping.ConfigSerializable;
+import org.spongepowered.configurate.objectmapping.meta.Comment;
 
 import java.util.List;
 import java.util.function.Supplier;
@@ -17,12 +18,15 @@ import java.util.function.Supplier;
 @Accessors(fluent = true)
 public class EventSection {
     private KickEventSection kickEvent = new KickEventSection();
+    @Comment("How many game minutes will a player be passed out?")
     private int passOutTime = 5;
+    @Comment("Drunken messages to send if the drunk_message event is enabled (not recommended to have enabled, gets a bit spammy)")
     private List<String> drunkMessages = List.of("I love you <random_player_name>, you're my best friend.",
             "I could do one more.",
             "Who is she?",
             "Watch this!",
             "I'm not drunk. You're drunk.");
+    @Comment("Make your own events, see the wiki at https://hangar.papermc.io/BreweryTeam/TheBrewingProject/pages/Wiki/Configuration#-events")
     private CustomEventRegistry customEvents = CustomEventRegistry.builder()
             .addEvent(new CustomEvent.Builder(BreweryKey.parse("memory_loss"))
                     .alcoholRequirement(60)
@@ -39,9 +43,14 @@ public class EventSection {
                                     new Interval(1, 1), new Interval(20 * Moment.SECOND, 20 * Moment.SECOND)
                             )).build()
             ).build();
+    @Comment("What events will be randomly chosen over time when the player is drunk")
     private List<String> enabledRandomEvents = List.of("puke", "memory_loss", "stumble", "chicken", "nausea", "tunnel_vision", "drunken_walk");
+    @Comment("Teleport destinations for the 'teleport' event")
     private List<Supplier<BreweryLocation>> teleportDestinations = List.of();
+    @Comment("Deny joining the server if too drunk")
     private boolean drunkenJoinDeny = true;
+    @Comment("Transform text with blurred speech if the player is drunk enough")
+    private boolean blurredSpeech = true;
 
     @ConfigSerializable
     @Getter
