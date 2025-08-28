337b74a3f1873ed0ca0dd51c66d991f80204742b
Mitality <96748385+Mitality@users.noreply.github.com>
2025-08-28 14:01:31 +0200

    feat(time-formatting): Yes, it can get more advanced :)

--- bukkit/src/main/java/dev/jsinco/brewery/bukkit/util/TimeUtil.java
+++ bukkit/src/main/java/dev/jsinco/brewery/bukkit/util/TimeUtil.java
@@ -11,23 +11,56 @@ public class TimeUtil {
         String format = Config.config().timeFormat();
 
         long cookingMinuteTicks = Config.config().cauldrons().cookingMinuteTicks();
-        long totalMinutes = timeTicks / cookingMinuteTicks;
 
+        long totalTicks = timeTicks;
+        long totalSeconds = timeTicks * 60 / cookingMinuteTicks;
+        long totalMinutes = timeTicks / cookingMinuteTicks;
+        long totalHours = totalMinutes / 60;
+        long totalDays = totalHours / 24;
+
+        long ticks = timeTicks % cookingMinuteTicks;
         long seconds = (timeTicks % cookingMinuteTicks) * 60 / cookingMinuteTicks;
         long minutes = totalMinutes % 60;
-        long hours = totalMinutes / 60;
+        long hours = totalHours % 24;
+        long days = totalDays;
 
         String result = format;
 
-        result = result.replace("<hours>", String.valueOf(hours))
-                .replace("<minutes>", String.valueOf(minutes))
+        result = result
+                .replace("<ticks>", String.valueOf(ticks))
+                .replace("<ticks-total>", String.valueOf(totalTicks))
                 .replace("<seconds>", String.valueOf(seconds))
-                .replace("<ticks>", String.valueOf(timeTicks));
+                .replace("<seconds-total>", String.valueOf(totalSeconds))
+                .replace("<minutes>", String.valueOf(minutes))
+                .replace("<minutes-total>", String.valueOf(totalMinutes))
+                .replace("<hours>", String.valueOf(hours))
+                .replace("<hours-total>", String.valueOf(totalHours))
+                .replace("<days>", String.valueOf(days))
+                .replace("<days-total>", String.valueOf(totalDays));
 
-        result = result.replaceAll("(?s)<if-hours>(.*?)</if-hours>", hours > 0 ? "$1" : "")
-                .replaceAll("(?s)<if-minutes>(.*?)</if-minutes>", minutes > 0 ? "$1" : "")
+        result = result
+                .replaceAll("(?s)<if-ticks>(.*?)</if-ticks>", ticks > 0 ? "$1" : "")
+                .replaceAll("(?s)<if-ticks-total>(.*?)</if-ticks-total>", totalTicks > 0 ? "$1" : "")
                 .replaceAll("(?s)<if-seconds>(.*?)</if-seconds>", seconds > 0 ? "$1" : "")
-                .replaceAll("(?s)<if-ticks>(.*?)</if-ticks>", timeTicks > 0 ? "$1" : "");
+                .replaceAll("(?s)<if-seconds-total>(.*?)</if-seconds-total>", totalSeconds > 0 ? "$1" : "")
+                .replaceAll("(?s)<if-minutes>(.*?)</if-minutes>", minutes > 0 ? "$1" : "")
+                .replaceAll("(?s)<if-minutes-total>(.*?)</if-minutes-total>", totalMinutes > 0 ? "$1" : "")
+                .replaceAll("(?s)<if-hours>(.*?)</if-hours>", hours > 0 ? "$1" : "")
+                .replaceAll("(?s)<if-hours-total>(.*?)</if-hours-total>", totalHours > 0 ? "$1" : "")
+                .replaceAll("(?s)<if-days>(.*?)</if-days>", days > 0 ? "$1" : "")
+                .replaceAll("(?s)<if-days-total>(.*?)</if-days-total>", totalDays > 0 ? "$1" : "");
+
+        result = result
+                .replaceAll("(?s)<if-!ticks>(.*?)</if-!ticks>", ticks > 0 ? "" : "$1")
+                .replaceAll("(?s)<if-!ticks-total>(.*?)</if-!ticks-total>", totalTicks > 0 ? "" : "$1")
+                .replaceAll("(?s)<if-!seconds>(.*?)</if-!seconds>", seconds > 0 ? "" : "$1")
+                .replaceAll("(?s)<if-!seconds-total>(.*?)</if-!seconds-total>", totalSeconds > 0 ? "" : "$1")
+                .replaceAll("(?s)<if-!minutes>(.*?)</if-!minutes>", minutes > 0 ? "" : "$1")
+                .replaceAll("(?s)<if-!minutes-total>(.*?)</if-!minutes-total>", totalMinutes > 0 ? "" : "$1")
+                .replaceAll("(?s)<if-!hours>(.*?)</if-!hours>", hours > 0 ? "" : "$1")
+                .replaceAll("(?s)<if-!hours-total>(.*?)</if-!hours-total>", totalHours > 0 ? "" : "$1")
+                .replaceAll("(?s)<if-!days>(.*?)</if-!days>", days > 0 ? "" : "$1")
+                .replaceAll("(?s)<if-!days-total>(.*?)</if-!days-total>", totalDays > 0 ? "" : "$1");
 
         Pattern pluralPattern = Pattern.compile("\\[(.*?)\\]");
         Matcher matcher = pluralPattern.matcher(result);
