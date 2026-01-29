fb868e352e26d9cb657fe9dc26144af80eaa63b7
Hjalmar Gunnarsson <officialhjalmar.gunnarsson@outlook.com>
2026-01-29 11:36:14 +0100

    feat(cauldron): Add randomness to the animation effect

--- bukkit/src/main/java/dev/jsinco/brewery/bukkit/animation/ItemAddAnimation.java
+++ bukkit/src/main/java/dev/jsinco/brewery/bukkit/animation/ItemAddAnimation.java
@@ -7,28 +7,45 @@ import org.bukkit.util.Transformation;
 import org.bukkit.util.Vector;
 import org.joml.AxisAngle4f;
 
+import java.util.Random;
 import java.util.function.Consumer;
 
 public class ItemAddAnimation implements Consumer<ScheduledTask> {
 
     private final Location from;
     private final Location to;
-    private final Vector offsetDirection;
+    private final Vector offset;
+    private final Vector travelingDirection;
     private final ItemDisplay entity;
     private long time = 0;
+    private double v0x;
+    private float startingRotation = (float) RANDOM.nextDouble(Math.PI * 2);
+    private static final Random RANDOM = new Random();
     private static final double Y_MAX = 1D;
     private static final double X_END = 1D;
     private static final double G = 9.82;
     private static final double V0Y = Math.sqrt(Y_MAX * G * 2);
-    private static final double V0X = G * X_END / (V0Y * 2);
-    public static final long T_END = (long) Math.ceil(X_END / V0X * 20);
+    public static final long T_END = (long) Math.ceil((V0Y * 2) / G * 20);
 
     public ItemAddAnimation(Location from, Location to, ItemDisplay entity) {
-        this.to = to;
-        this.entity = entity;
+        double xRandom = RANDOM.nextDouble(-0.4, 0.4);
+        double zRandom = RANDOM.nextDouble(-0.4, 0.4);
         double yawRadians = (from.getYaw() - 90) / 360 * Math.PI * 2;
-        this.offsetDirection = new Vector(Math.cos(yawRadians), 0, Math.sin(yawRadians));
-        this.from = to.clone().add(offsetDirection.clone().multiply(X_END));
+        Vector randomZX = new Vector(
+                Math.cos(yawRadians) * (xRandom) + Math.sin(yawRadians) * zRandom,
+                0,
+                Math.sin(yawRadians) * (xRandom) - Math.cos(yawRadians) * zRandom
+        );
+        this.offset = new Vector(
+                Math.cos(yawRadians) * X_END + RANDOM.nextDouble(-0.1, 0.1),
+                0,
+                Math.sin(yawRadians) * X_END + RANDOM.nextDouble(-0.1, 0.1)
+        );
+        this.travelingDirection = offset.clone().normalize().multiply(-1);
+        this.v0x = G * (offset.clone().subtract(randomZX)).length() / (V0Y * 2);
+        this.to = to.clone().add(randomZX);
+        this.entity = entity;
+        this.from = to.clone().add(offset);
         /*
          * Equation definitions are defined within square brackets []
          * Equation of movement under gravity in 2d
@@ -65,14 +82,13 @@ public class ItemAddAnimation implements Consumer<ScheduledTask> {
         }
         Transformation previous = entity.getTransformation();
         entity.setTransformation(
-                new Transformation(previous.getTranslation(), previous.getLeftRotation().rotationAxis(new AxisAngle4f((float) Math.PI * time / 10, 0, 1, 0)), previous.getScale(), previous.getRightRotation())
+                new Transformation(previous.getTranslation(), previous.getLeftRotation().rotationAxis(new AxisAngle4f((float) Math.PI * time / 10 + startingRotation, 0, 1, 0)), previous.getScale(), previous.getRightRotation())
         );
         time++;
         double timeSeconds = time / 20D;
-        Vector travelingPoint = offsetDirection.clone().multiply(-1);
-        travelingPoint.multiply(timeSeconds * V0X).setY(
-                (V0Y - G * timeSeconds / 2) * timeSeconds
-        );
+        Vector travelingPoint = travelingDirection.clone()
+                .multiply(timeSeconds * v0x)
+                .setY((V0Y - G * timeSeconds / 2) * timeSeconds);
         entity.teleport(from.clone().add(travelingPoint));
     }
 }
