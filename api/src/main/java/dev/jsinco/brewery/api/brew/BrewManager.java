f2014685a337d580d99790964fbfff91d24b69db
Hjalmar Gunnarsson <officialhjalmar.gunnarsson@outlook.com>
2026-03-09 09:03:46 +0100

    feat(brew-api): Expose creation of brewing steps

--- api/src/main/java/dev/jsinco/brewery/api/brew/BrewManager.java
+++ api/src/main/java/dev/jsinco/brewery/api/brew/BrewManager.java
@@ -1,8 +1,12 @@
 package dev.jsinco.brewery.api.brew;
 
+import dev.jsinco.brewery.api.breweries.BarrelType;
+import dev.jsinco.brewery.api.breweries.CauldronType;
+import dev.jsinco.brewery.api.ingredient.Ingredient;
 import dev.jsinco.brewery.api.meta.MetaData;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 public interface BrewManager<I> {
@@ -39,4 +43,33 @@ public interface BrewManager<I> {
      * @return An optionally present brew if item had brew contents
      */
     Optional<Brew> fromItem(I item);
+
+    /**
+     * @param cookingTicks The ticks spent cooking
+     * @param ingredients  The added ingredients
+     * @param cauldronType The type of cauldron used in the process
+     * @return A new cooking step
+     */
+    BrewingStep.Cook cookingStep(long cookingTicks, Map<? extends Ingredient, Integer> ingredients, CauldronType cauldronType);
+
+    /**
+     * @param mixingTicks  The ticks spent mixing
+     * @param ingredients  The added ingredients
+     * @param cauldronType The type of cauldron used in the process (ignored at the moment)
+     * @return A new mixing step
+     */
+    BrewingStep.Mix mixingStep(long mixingTicks, Map<? extends Ingredient, Integer> ingredients, CauldronType cauldronType);
+
+    /**
+     * @param agingTicks The ticks spent aging
+     * @param barrelType The type of barrel used in the process
+     * @return A new aging step
+     */
+    BrewingStep.Age agingStep(long agingTicks, BarrelType barrelType);
+
+    /**
+     * @param runs The amount of distill runs
+     * @return A new distill step
+     */
+    BrewingStep.Distill distillStep(int runs);
 }
--- bukkit/src/main/java/dev/jsinco/brewery/bukkit/brew/BukkitBrewManager.java
+++ bukkit/src/main/java/dev/jsinco/brewery/bukkit/brew/BukkitBrewManager.java
@@ -1,13 +1,18 @@
 package dev.jsinco.brewery.bukkit.brew;
 
 import dev.jsinco.brewery.api.brew.Brew;
-import dev.jsinco.brewery.api.meta.MetaData;
-import dev.jsinco.brewery.brew.BrewImpl;
 import dev.jsinco.brewery.api.brew.BrewManager;
 import dev.jsinco.brewery.api.brew.BrewingStep;
+import dev.jsinco.brewery.api.breweries.BarrelType;
+import dev.jsinco.brewery.api.breweries.CauldronType;
+import dev.jsinco.brewery.api.ingredient.Ingredient;
+import dev.jsinco.brewery.api.meta.MetaData;
+import dev.jsinco.brewery.api.moment.PassedMoment;
+import dev.jsinco.brewery.brew.*;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 public class BukkitBrewManager implements BrewManager<ItemStack> {
@@ -39,5 +44,35 @@ public class BukkitBrewManager implements BrewManager<ItemStack> {
     @Override
     public Optional<Brew> fromItem(ItemStack item) {
         return BrewAdapter.fromItem(item);
+    }
+
+    @Override
+    public BrewingStep.Cook cookingStep(long cookingTicks, Map<? extends Ingredient, Integer> ingredients, CauldronType cauldronType) {
+        return new CookStepImpl(
+                new PassedMoment(cookingTicks),
+                ingredients,
+                cauldronType
+        );
+    }
+
+    @Override
+    public BrewingStep.Mix mixingStep(long mixingTicks, Map<? extends Ingredient, Integer> ingredients, CauldronType cauldronType) {
+        return new MixStepImpl(
+                new PassedMoment(mixingTicks),
+                ingredients
+        );
+    }
+
+    @Override
+    public BrewingStep.Age agingStep(long agingTicks, BarrelType barrelType) {
+        return new AgeStepImpl(
+                new PassedMoment(agingTicks),
+                barrelType
+        );
+    }
+
+    @Override
+    public BrewingStep.Distill distillStep(int runs) {
+        return new DistillStepImpl(runs);
     }
 }
